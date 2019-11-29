4b3a48a723e829803a6918bc6afee2ca5afd4066
Aaron <rustinante@gmail.com>
2019-11-28 20:27:38 -0800

    check valid topo order and detect duplicates

--- tests/test_example_repos.py
+++ tests/test_example_repos.py
@@ -7,16 +7,48 @@ from topo_order_commits import topo_order_commits
 
 
 @pytest.mark.parametrize("repo_id", ['1'])
-def test_repo(repo_id, capsys):
+def test_topo_order_constraint(repo_id, capsys):
     repo_fixture_dir = os.path.join('tests', 'repo_fixture')
     repo_name = f'example-repo-{repo_id}'
     if not os.path.exists(os.path.join(repo_fixture_dir, repo_name)):
         tar = tarfile.open(os.path.join(repo_fixture_dir, f'{repo_name}.tar.gz'))
         tar.extractall(path=repo_fixture_dir)
         tar.close()
+
+    child_to_parent_edges = get_child_to_parent_edges(os.path.join(repo_fixture_dir, f'{repo_name}-edges.txt'))
+
     cwd = os.getcwd()
     os.chdir(os.path.join(repo_fixture_dir, repo_name))
     topo_order_commits()
-    output = capsys.readouterr()
-    assert output.out.strip() == 'hi'
+
+    output_lines = capsys.readouterr().out.strip().split('\n')
+    assigned_commit_order = assign_commit_order_and_detect_duplicates(output_lines)
+
+    for edge in child_to_parent_edges:
+        child, parent = edge
+
+        if child not in assigned_commit_order:
+            raise Exception(f'Missing commit hash {child}')
+        if parent not in assigned_commit_order:
+            raise Exception(f'Missing commit hash {parent}')
+
+        assert assigned_commit_order[child] < assigned_commit_order[parent], \
+            f'{child} has to precede {parent} in a topological order'
+
     os.chdir(cwd)
+
+
+def assign_commit_order_and_detect_duplicates(output_lines):
+    assigned_order = {}
+    for line in output_lines:
+        if line and '=' not in line:
+            commit_hash = line.split()[0]
+            if commit_hash in assigned_order:
+                raise Exception(f'Duplicate commits detected. Commit hash: {commit_hash}')
+            assigned_order[commit_hash] = len(assigned_order)
+    return assigned_order
+
+
+def get_child_to_parent_edges(filepath):
+    with open(filepath, 'r') as file:
+        return [line.split() for line in file]
