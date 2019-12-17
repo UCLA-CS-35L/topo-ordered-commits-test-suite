099265d106d72ae836e4ad3e0f93d6e80516632c
Aaron <rustinante@gmail.com>
2019-12-17 15:24:10 -0800

    timeout

--- tests/test_example_repos.py
+++ tests/test_example_repos.py
@@ -8,6 +8,7 @@ from topo_order_commits import topo_order_commits
 
 
 @pytest.mark.parametrize("repo_id", [1, 2, 3, 4, 5, 6, 7])
+@pytest.mark.timeout(10)
 def test_topo_order_constraint(repo_id, capsys):
     run_topo_order_commits_on_repo(repo_id)
     child_to_parent_edges = get_child_to_parent_edges(repo_id)
@@ -28,6 +29,7 @@ def test_topo_order_constraint(repo_id, capsys):
 
 
 @pytest.mark.parametrize("repo_id", [1, 2, 3, 4, 5, 6, 7])
+@pytest.mark.timeout(10)
 def test_sticky_starts_and_ends(repo_id, capsys):
     run_topo_order_commits_on_repo(repo_id)
     child_to_parent_edges = get_child_to_parent_edges(repo_id)
@@ -101,6 +103,7 @@ def test_sticky_starts_and_ends(repo_id, capsys):
 
 
 @pytest.mark.parametrize("repo_id", [1, 2, 3, 4, 5, 6, 7])
+@pytest.mark.timeout(10)
 def test_branch_heads(repo_id, capsys):
     run_topo_order_commits_on_repo(repo_id)
     output_lines = capsys.readouterr().out.strip().split('\n')
