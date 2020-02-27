3d4e7bdb9efc529f0dc521fa13c06ea8ba267809
Aaron <rustinante@gmail.com>
2020-02-26 17:15:22 -0800

    more details in the comment

--- topo_order_commits.py
+++ topo_order_commits.py
@@ -1,5 +1,14 @@
 # Keep the function signature,
 # but replace its body with your implementation
+#
+# Note that this is the driver function.
+# Please write a well-structured implemention by creating other functions
+# each of which has a designated purpose.
+#
+# As a good programming practice,
+# please do not use any script-level variables that are modifiable.
+# This is because those variables live on forever once the script is imported,
+# and the changes to them will persist across different invocations of the imported functions
 def topo_order_commits():
     raise NotImplementedError
 
